0b1a3d931990634fe540ad6be3736a71ae095cc9
Khoi Le <khoileee89@gmail.com>
2017-01-15 17:04:16 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DrumKit from './DrumKit.js';
+import './index.css';
+
+ReactDOM.render(
+  <DrumKit />,
+  document.getElementById('root')
+);
